ca1e2b95d6d6389c9627596f547193a273d5314f
Daria Semenova <dasha.ign,semenova@gmail.com>
2023-10-03 13:55:01 +0300

    при смене коллекции не генерируются повторно колонки,

--- DynamicDataGrid/ViewModels/MainViewModel.cs
+++ DynamicDataGrid/ViewModels/MainViewModel.cs
@@ -2,23 +2,26 @@
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using System.Windows.Markup;
-
+using DynamicDataGrid.Services;
 using MathCore.WPF.Commands;
 using MathCore.WPF.ViewModels;
+using Ookii.Dialogs.Wpf;
 
 namespace DynamicDataGrid.ViewModels;
 
 [MarkupExtensionReturnType(typeof(MainViewModel))]
 public class MainViewModel() : TitledViewModel("Главное окно")
 {
+    private readonly IFileService _fileService = new FileService();
+
     private ObservableCollection<DataViewModel> _Files
-    //    = new()
-    //{
-    //    new("File 1", @"c:\data\data_file1.txt"),
-    //    new("File 2", @"c:\data\d1\data_file2.txt"),
-    //    new("File 3", @"c:\data\d2\d3\data_file3.txt"),
-    //    new("File 4", @"c:\data\d3\d5\d7\d8\data_file4.txt"),
-    //}
+        = new()
+    {
+        new("File 1", @"c:\data\data_file1.txt"),
+        new("File 2", @"c:\data\d1\data_file2.txt"),
+        new("File 3", @"c:\data\d2\d3\data_file3.txt"),
+        new("File 4", @"c:\data\d3\d5\d7\d8\data_file4.txt"),
+    }
         ;
 
     public ObservableCollection<DataViewModel> Files { get => _Files; set => Set(ref _Files!, value); }
@@ -78,6 +81,22 @@ public class MainViewModel() : TitledViewModel("Главное окно")
 
         OnPropertyChanged(nameof(Files));
 
+    }
+
+
+    private Command? _ChangeRootFolderCommand;
+
+    public ICommand ChangeRootFolderCommand => _ChangeRootFolderCommand ??= Command.New(() => ChangeRootFolder());
+
+    private async void ChangeRootFolder()
+    {
+        var dialog = new VistaFolderBrowserDialog { Multiselect = false };
+        if (!dialog.ShowDialog() == true)
+            return;
+
+        _Files = new ObservableCollection<DataViewModel>(await _fileService.CreateItemList(dialog.SelectedPath));
+
+        OnPropertyChanged(nameof(Files));
     }
 
 
